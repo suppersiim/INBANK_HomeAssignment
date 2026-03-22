d9c9aca94bc9748ed7b067bce805aacbe31c8eab
Siim Ilison <ilison@ut.ee>
2026-03-22 19:04:44 +0200

    Made a class LoanDecision for the final decision

---
+++ src/LoanDecision.java
@@ -0,0 +1,12 @@
+public class LoanDecision {
+
+    private final String status;
+    private final Integer loanAmount;
+    private final Integer loanPeriod;
+
+    public LoanDecision(String status, Integer loanAmount, Integer loanPeriod) {
+        this.status = status;
+        this.loanAmount = loanAmount;
+        this.loanPeriod = loanPeriod;
+    }
+}
