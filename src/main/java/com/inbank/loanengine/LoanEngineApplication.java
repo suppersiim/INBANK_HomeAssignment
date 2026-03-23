e9fd6ad4409dee81e11be491096cdd287abad08b
Siim Ilison <ilison@ut.ee>
2026-03-23 11:27:22 +0200

    Added Spring Boot annotation

--- src/main/java/com/inbank/loanengine/LoanEngineApplication.java
+++ src/main/java/com/inbank/loanengine/LoanEngineApplication.java
@@ -1,4 +1,11 @@
 package com.inbank.loanengine;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class LoanEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoanEngineApplication.class, args);
+    }
 }
