9ef8270f7bb8c907ca51bf04801203260a838ace
Siim Ilison <ilison@ut.ee>
2026-03-23 14:53:59 +0200

    Corrected and reformated code

--- src/main/java/com/inbank/loanengine/controller/LoanDecisionController.java
+++ src/main/java/com/inbank/loanengine/controller/LoanDecisionController.java
@@ -5,10 +5,6 @@ public class LoanDecisionController {
     private final String personalCode;
     private final double loanAmount;
     private final int loanPeriod;
-    private final int minimumLoanAmount = 2000;
-    private final int maximumLoanAmount = 10000;
-    private final int minimumLoanPeriod = 12;
-    private final int maximumLoanPeriod = 60;
 
     public LoanDecisionController(String personalCode, double loanAmount, int loanPeriod) {
         this.personalCode = personalCode;
@@ -29,32 +25,33 @@ public class LoanDecisionController {
     }
 
     public int getMinimumLoanAmount() {
-        return minimumLoanAmount;
+        return 2000;
     }
 
     public int getMaximumLoanAmount() {
-        return maximumLoanAmount;
+        return 10000;
     }
 
     public int getMinimumLoanPeriod() {
-        return minimumLoanPeriod;
+        return 12;
     }
 
     public int getMaximumLoanPeriod() {
-        return maximumLoanPeriod;
+        return 60;
     }
 
     // getCreaditScore method determines if person has debt (return null) and if not returns the credit score of the
     // person
-    public Integer getCreditScore(){
+    public Integer getCreditScore() throws Exception {
         if (getPersonalCode().equals("49002010965")) return null;
         else if (getPersonalCode().equals("49002010976")) return 100;
         else if (getPersonalCode().equals("49002010987")) return 300;
         else if (getPersonalCode().equals("49002010998")) return 1000;
+        else throw new Exception();
     }
 
     // InputController method validates that the input is in the correct (returns true) and if not returns false
-    public boolean InputController(){
+    public boolean InputController() throws Exception {
 
         if (getCreditScore() != null &&
             getLoanAmount() >= getMinimumLoanAmount() && getLoanAmount() <= getMaximumLoanAmount() &&
