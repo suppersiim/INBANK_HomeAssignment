488ba62a0492c31fbc80b92d19cf4c67c376e372
Siim Ilison <ilison@ut.ee>
2026-03-23 11:20:40 +0200

    Created a class that determines if a person has debt and if not returns the correct credit score based on the personal code

--- src/main/java/com/inbank/loanengine/domain/CustomerSegment.java
+++ src/main/java/com/inbank/loanengine/domain/CustomerSegment.java
@@ -1,4 +1,20 @@
 package com.inbank.loanengine.domain;
 
 public class CustomerSegment {
+
+    private final int creditModifier;
+
+    final Integer debt = null;
+    final Integer segment_1 = 100;
+    final Integer segment_2 = 300;
+    final Integer segment_3 = 1000;
+
+
+    public CustomerSegment(int creditModifier) {
+        this.creditModifier = creditModifier;
+    }
+
+    public int getCreditModifier() {
+        return creditModifier;
+    }
 }
