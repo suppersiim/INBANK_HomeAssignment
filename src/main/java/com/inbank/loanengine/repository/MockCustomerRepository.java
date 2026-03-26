5f0a0f2b9ee726f6aab15cf38af7509f55af5560
Siim Ilison <ilison@ut.ee>
2026-03-26 15:55:43 +0200

    changed debt from null to 0

--- src/main/java/com/inbank/loanengine/repository/MockCustomerRepository.java
+++ src/main/java/com/inbank/loanengine/repository/MockCustomerRepository.java
@@ -1,19 +1,21 @@
 package com.inbank.loanengine.repository;
 
 import com.inbank.loanengine.domain.CustomerSegment;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class MockCustomerRepository implements CustomerRepository{
 
     // method determines if person has debt (return null) and if not returns the credit score of the person
     @Override
     public CustomerSegment findByPersonalCode(String personalCode) throws Exception {
 
-        if (personalCode.equals("49002010965")) return null;
+        if (personalCode.equals("49002010965")) return new CustomerSegment(0);
         else if (personalCode.equals("49002010976")) return new CustomerSegment(100);
         else if (personalCode.equals("49002010987")) return new CustomerSegment(300);
         else if (personalCode.equals("49002010998")) return new CustomerSegment(1000);
         else {
-            System.out.println("ERROR - PERSONAL CODE NOT MATCHING");
+            //System.out.println("ERROR - PERSONAL CODE NOT MATCHING");
             throw new Exception();
         }
     }
