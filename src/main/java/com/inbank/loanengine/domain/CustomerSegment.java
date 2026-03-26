bd8c29f8036846b41087ec88fe28843d40ba4a04
Siim Ilison <ilison@ut.ee>
2026-03-26 15:53:02 +0200

    Modified CustomerSegment file

--- src/main/java/com/inbank/loanengine/domain/CustomerSegment.java
+++ src/main/java/com/inbank/loanengine/domain/CustomerSegment.java
@@ -3,12 +3,12 @@ package com.inbank.loanengine.domain;
 public class CustomerSegment {
 
     private final int creditModifier;
-
-    final Integer debt = null;
-    final Integer segment_1 = 100;
-    final Integer segment_2 = 300;
-    final Integer segment_3 = 1000;
-
+/*
+    final int debt = 0;
+    final int segment_1 = 100;
+    final int segment_2 = 300;
+    final int segment_3 = 1000;
+*/
 
     public CustomerSegment(int creditModifier) {
         this.creditModifier = creditModifier;
